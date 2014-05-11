d830827fcc4d3cccc239f5c652107c65597e8112
dw40485399 <dw40485399@gmail.com>
2014-05-10 21:02:42 -0400

    plot 4 added

---
+++ plot4.R
@@ -0,0 +1,32 @@
+
+      ## Read Household Power data
+      file <- "household_power_consumption.txt"
+      dataset <- read.table(file,header=TRUE,sep=";",colClasses="character",na.strings="?")
+      dataset <- rbind(dataset[dataset$Date=="1/2/2007",],dataset[dataset$Date=="2/2/2007",])
+      dataset[,7] <- as.numeric(dataset[,7])
+      dataset[,8] <- as.numeric(dataset[,8])
+      dataset[,9] <- as.numeric(dataset[,9])
+      
+      ## Create a date field with time
+      datetime <- paste(dataset[,1],dataset[,2])
+      datetime <- strptime(datetime,format="%d/%m/%Y %H:%M:%S")
+      dataset <- cbind(dataset,datetime)
+      
+      ## Generate the plot
+      png("plot4.png")
+      par(mfcol=c(2,2))      
+      plot(dataset[,10],dataset[,3],type="l",xlab="",ylab="Global Active Power")
+      
+      plot(dataset[,10],dataset[,7],type="n",xlab="",ylab="Energy sub metering")
+      points(dataset[,10],dataset[,7],type="l")
+      points(dataset[,10],dataset[,8],type="l",col="red")
+      points(dataset[,10],dataset[,9],type="l",col="blue")
+      legend("topright",names(dataset[,7:9]),col=c("black","red","blue"),lty=1,bty="n",cex=.75)
+      
+      plot(dataset[,10],dataset[,5],type="l",xlab="datetime",ylab="Voltage")
+      
+      plot(dataset[,10],dataset[,4],type="l",xlab="datetime",ylab="Global_reactive_power")
+      
+      
+      
+      dev.off() 
